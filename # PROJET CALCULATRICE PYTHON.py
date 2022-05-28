78cef57c7a4747d5d586e8226560617e2736f344
FrairMaxou <maxime.gosselin.pro@gmail.com>
2022-05-28 15:41:37 +0900

    Initial commit

---
+++ # PROJET CALCULATRICE PYTHON.py
@@ -0,0 +1,11 @@
+# PROJET CALCULATRICE PYTHON
+
+# Demander un premier chiffre
+# Demander un second chiffre
+# Faire une addition
+# Display le résultat de l'addition
+
+premier_chiffre = input("Veuillez entrer un premier nombre :")
+second_chiffre = input("Veuilez entrer un second chiffre :")
+resultat = float(premier_chiffre) + float(second_chiffre)
+print(f"Le résultat de l'addition du nombre {premier_chiffre} avec le nombre {second_chiffre} est égal à {resultat}")
